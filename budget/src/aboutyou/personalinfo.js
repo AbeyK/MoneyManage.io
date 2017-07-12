eb57c2bd7aca161bd279700cf07eab75a7e83c39
jmcacioppo <jcacioppo@pietech.com>
2017-07-12 13:19:12 -0400

    Added drag and drop, changed nav bar

--- budget/src/aboutyou/personalinfo.js
+++ budget/src/aboutyou/personalinfo.js
@@ -11,6 +11,36 @@ export class personalinfo {
         this.slider = slider;
     }
 
+    //DRAG AND DROP
+    allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    drag(ev) {
+        ev.dataTransfer.setData("tonberry", ev.target.innerText);
+        ev.dataTransfer.setData("occu-name", ev.srcElement.textContent);
+        return true;
+    }
+
+    removeDrop(ev) {
+        ev.dataTransfer.set
+    }
+
+    drop(ev) {
+        ev.preventDefault();
+        var current;
+        var data = ev.dataTransfer.getData("tonberry");
+        var elements = document.getElementsByClassName("current-buttons");
+        var occupationName;
+        for(var i = 0; i < elements.length; i++) {
+            if((elements[i].textContent).trim() === data.trim()) {
+                current = elements[i];
+                occupationName = elements[i].textContent.trim();
+            }
+        }
+        ev.currentTarget.appendChild(current);
+    }
+
     attached() {
         this.slider.createAgeSlider();
     }
