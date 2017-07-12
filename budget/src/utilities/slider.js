50b0f029eeb8093faa135a73296ceb2c07978b47
jmcacioppo <jcacioppo@PIE_MAIN.pietech.com>
2017-07-12 12:26:51 -0400

    Added some inputs on personal info, made slider.js in new utilities folder

---
+++ budget/src/utilities/slider.js
@@ -0,0 +1,23 @@
+import {inject} from 'aurelia-framework';
+import {User} from '../services/user';
+import * as ionRangeSlider from "ion-rangeslider";
+
+@inject(User)
+export class Slider {
+    constructor(user) {
+        this.user = user;
+    }
+    
+    createAgeSlider() {
+        $("#age").ionRangeSlider({
+            grid: true,
+            min: 0,
+            max: 100,
+            from: this.user.personalInfo.age,
+            step: 1,
+            onFinish: (data) => {
+                this.user.personalInfo.age = data.from;
+            }
+        });
+    }
+}
