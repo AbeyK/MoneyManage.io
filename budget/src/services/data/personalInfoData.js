681c02d03f989212d2e98d7c272c2a265703496b
jmcacioppo <jcacioppo@PIE_MAIN.pietech.com>
2017-07-13 12:24:16 -0400

    Drag and drop is fully functional, needs styling. Made Personal Info with composing

--- budget/src/services/data/personalInfoData.js
+++ budget/src/services/data/personalInfoData.js
@@ -6,25 +6,25 @@ export class PersonalInfoData {
         this.goalsList=["Private School", "College", "Wedding", "Vacation",
             "Boat", "New Car", "Other"];
         this.checkSchool = false;
-        this.privateSchool = 0;
+        this.PrivateSchool = 0;
 
         this.checkCollege = false;
-        this.college = 0;
+        this.College = 0;
 
         this.checkWedding = false;
-        this.wedding = 0;
+        this.Wedding = 0;
 
         this.checkVacation = false;
-        this.vacation = 0;
+        this.Vacation = 0;
 
         this.checkBoat = false;
-        this.boat = 0;
+        this.Boat = 0;
 
         this.checkCar = false;
-        this.newCar = 0;
+        this.NewCar = 0;
 
         this.checkOther = false;
-        this.other = 0;
+        this.Other = 0;
         
         this.currentGoals = [];
     }
