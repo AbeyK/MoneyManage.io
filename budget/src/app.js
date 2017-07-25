a96c74784b2d2cbe2fe89a6fad786106e427e87f
Abey Koolipurackal <abey@virginia.edu>
2017-07-25 15:49:51 -0400

    fixed bugs, set up about page

--- budget/src/app.js
+++ budget/src/app.js
@@ -16,32 +16,32 @@ export class App {
     config.map([
       {
         route: ['', 'home'], moduleId: 'home/home',
-        name: 'home', title: ' MoneyManage: Budgeting Tool ', nav: false
+        name: 'home', title: ' MoneyManage: Budgeting Tool ', nav: false, settings: 'Home'
       },
       {
         route: 'personalinfo', moduleId: 'aboutyou/personalinfo',
-        name: 'personalinfo', title: 'MoneyManage: Personal Info', nav: true
+        name: 'personalinfo', title: 'MoneyManage: Personal Info', nav: true, settings: 'Personal'
       },
       {
         route: 'expenses', moduleId: 'expenses/expenses',
-        name: 'expenses', title: 'MoneyManage: Spending Habits', nav: true
+        name: 'expenses', title: 'MoneyManage: Spending Habits', nav: true, settings: 'Spending'
       },
 
       {
         route: 'results', moduleId: 'results/results',
-        name: 'results', title: 'MoneyManage: Results', nav: true
+        name: 'results', title: 'MoneyManage: Results', nav: true, settings: 'Results'
       },
       {
         route: 'logout', moduleId: 'logout/logout',
-        name: 'logout', title: 'MoneyManage: Logout', nav: false
+        name: 'logout', title: 'MoneyManage: Logout', nav: false, settings: 'Logout'
       },
       {
         route: 'login', moduleId: 'login/login',
-        name: 'longin', title: 'MoneyManage: Login', nav: false
+        name: 'login', title: 'MoneyManage: Login', nav: false, settings: 'Login'
       },
       {
         route: 'about', moduleId: 'about/about',
-        name: 'about', title: 'MoneyManage: About', nav: false
+        name: 'about', title: 'MoneyManage: About', nav: false, settings: 'About'
       }
     ]);
   }
