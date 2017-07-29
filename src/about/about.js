989db40673d9050d69d47d46801540e5c92ace8c
Abey Koolipurackal <abey@virginia.edu>
2017-07-29 17:15:11 -0400

    minor fixes on result page buttons, new text on about page, less saturation on splash screen background

--- src/about/about.js
+++ src/about/about.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {User} from '../services/user';
+var bootbox = require('bootbox');
 
 @inject(Router, User)
 export class about {
